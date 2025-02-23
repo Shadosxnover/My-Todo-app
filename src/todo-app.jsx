d4da3670a8b547e0093e322b05b6039c8031783e
Shadosxnover <elhourirachid@gmail.com>
2025-02-23 23:51:54 +0000

    fixed add problem

--- src/todo-app.jsx
+++ src/todo-app.jsx
@@ -200,7 +200,7 @@ function TodoApp() {
                             onChange={(e) => setNewTask(e.target.value)}
                             placeholder="What needs to be done?"
                         />
-                        <div className="flex flex-wrap gap-4">
+                        <div className="flex flex-wrap items-center gap-4">
                             <select
                                 value={taskPriority}
                                 onChange={(e) => setTaskPriority(e.target.value)}
@@ -222,6 +222,17 @@ function TodoApp() {
                                 placeholder="Category"
                                 className="bg-gray-700/50 rounded-lg p-2"
                             />
+                            <motion.button
+                                whileHover={{ scale: 1.05 }}
+                                whileTap={{ scale: 0.95 }}
+                                type="submit"
+                                className="px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors duration-200 flex items-center space-x-2"
+                            >
+                                <span>Add Task</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clipRule="evenodd" />
+                                </svg>
+                            </motion.button>
                         </div>
                     </form>
                     <input
